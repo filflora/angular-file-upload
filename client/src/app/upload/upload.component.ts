857322ad4c3557fb0c6d8d822eb378a67701aaf9
Filip Flora <filipmihaly.flora@ge.com>
2019-01-10 10:40:04 +0100

    Added 'a' field to the formData for testing purposes

--- client/src/app/upload/upload.component.ts
+++ client/src/app/upload/upload.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { DialogComponent } from './dialog/dialog.component';
 import { UploadService } from './upload.service';
 
 @Component({
-  selector: 'app-upload',
-  templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+    selector: 'app-upload',
+    templateUrl: './upload.component.html',
+    styleUrls: ['./upload.component.css']
 })
-export class UploadComponent {
-  constructor(public dialog: MatDialog, public uploadService: UploadService) {}
+export class UploadComponent implements OnInit {
+    constructor(public dialog: MatDialog, public uploadService: UploadService) { }
 
-  public openUploadDialog() {
-    let dialogRef = this.dialog.open(DialogComponent, { width: '50%', height: '50%' });
-  }
+    ngOnInit() {
+        this.openUploadDialog();
+    }
+
+    public openUploadDialog() {
+        this.dialog.open(DialogComponent, { width: '50%', height: '50%' });
+    }
+
 }
